cb902295c7a921c7389603fb5a35f5ff52f1723c
hon2838 <50094669+hon2838@users.noreply.github.com>
2024-12-12 00:10:50 +0800

    Update generation time in SQL database file

--- database/soc_pms.sql
+++ database/soc_pms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2024 at 04:40 PM
+-- Generation Time: Dec 11, 2024 at 05:10 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
